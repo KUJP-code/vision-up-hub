f443c914dd1a2e95534b3cd9936e4e570d3c0845
Brett <bretttanner171@gmail.com>
2024-01-16 20:26:50 +0900

    Increment download_counter on download

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -3,6 +3,7 @@
 class FilesController < ApplicationController
   def show
     @file = ActiveStorage::Blob.find(params[:id])
+    @file.update(download_count: @file.download_count + 1)
     send_data @file.download, filename: @file.filename.to_s
   end
 
