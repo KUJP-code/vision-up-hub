98e424dae05133a78bed054f4af5d32d13c1bdd8
Brett Tanner <bretttanner171@gmail.com>
2024-01-17 12:14:36 +0900

    Modify for laptop dev env

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,3 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails";
 import "./controllers";
-import * as ActiveStorage from "@rails/activestorage";
-ActiveStorage.start();
