5cb89de255fe6fefc50cdaa6454838f0dbb85de4
Brett <bretttanner171@gmail.com>
2024-01-29 14:20:26 +0900

    Update User#is? to take multiple strings

--- app/models/users/user.rb
+++ app/models/users/user.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  TYPES = %w[Admin CurriculumManager SchoolManager OrgAdmin Sales Teacher].freeze
+  TYPES = %w[Admin Curriculum SchoolManager OrgAdmin Sales Teacher].freeze
 
   validates :name, presence: true
   validates :type, inclusion: { in: TYPES }
@@ -11,7 +11,7 @@ class User < ApplicationRecord
   devise :confirmable, :database_authenticatable, :lockable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  def is?(type)
-    self.type == type
+  def is?(*types)
+    types.include?(type)
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,26 @@ RSpec.describe User do
   it 'has a valid factory' do
     expect(build(:user)).to be_valid
   end
+
+  context 'when checking for role' do
+    it 'matches if user role matches a single string role' do
+      admin_user = build(:user, :admin)
+      expect(admin_user.is?('Admin')).to be true
+    end
+
+    it 'does not match if user role does not match single string role' do
+      admin_user = build(:user, :admin)
+      expect(admin_user.is?('Curriculum')).to be false
+    end
+
+    it 'matches matches any of an array of string roles' do
+      curriculum_user = build(:user, :curriculum)
+      expect(curriculum_user.is?('Admin', 'Curriculum')).to be true
+    end
+
+    it 'does not match if user role does not match any of an array of string roles' do
+      curriculum_user = build(:user, :curriculum)
+      expect(curriculum_user.is?('Admin', 'Teacher')).to be false
+    end
+  end
 end
