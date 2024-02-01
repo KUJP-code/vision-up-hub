484f2a4e16af280209a4a59de2417197c508d08b
Brett Tanner <bretttanner171@gmail.com>
2024-02-01 17:43:13 +0900

    Create TeachersController

---
+++ app/controllers/users/teachers_controller.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+class TeachersController < UsersController
+  def show; end
+
+  def new
+    @user = authorize Teacher.new(organisation_id: params[:organisation_id])
+  end
+
+  def create
+    @user = authorize Teacher.new(teachers_params)
+
+    if @user.save
+      redirect_to organisation_teacher_path(@user.organisation, @user),
+                  notice: t('create_success')
+    else
+      render :new,
+             status: :unprocessable_entity,
+             alert: t('create_failure')
+    end
+  end
+
+  def update
+    if @user.update(teachers_params)
+      redirect_to organisation_teacher_path(@user.organisation, @user),
+                  notice: t('update_success')
+    else
+      render :edit,
+             status: :unprocessable_entity,
+             alert: t('update_failure')
+    end
+  end
+
+  private
+
+  def org_schools
+    @user.organisation.schools.pluck(:name, :id)
+  end
+
+  def teachers_params
+    sm_params = [managements_attributes: %i[id school_id teacher_id _destroy]]
+    params.require(:teacher).permit(user_params + sm_params)
+  end
+
+  def scoped_users
+    super.where(type: 'Teacher')
+  end
+end
