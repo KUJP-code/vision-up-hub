a545c02f900da57fe9511d554813451ae0259eff
Brett <bretttanner171@gmail.com>
2024-02-01 16:41:54 +0900

    Write system spec for SM creating teacher

---
+++ spec/system/users/teacher_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'creating Teacher' do
+  let!(:organisation) { create(:organisation) }
+  let!(:school) { organisation.schools.create!(attributes_for(:school)) }
+
+  before do
+    sign_in create(:user, :school_manager, organisation:, schools: [school])
+  end
+
+  it 'School Manager can create teacher at their schools' do
+    visit organisation_teachers_path(organisation)
+    click_link 'create_teacher'
+    within '#teacher_form' do
+      fill_in 'teacher_name', with: 'John'
+      fill_in 'teacher_email', with: 'xjpjv@example.com'
+      select school.name, from: 'teacher_school_id'
+      click_on 'submit_teacher'
+    end
+    expect(page).to have_content 'John'
+    expect(teacher.school).to eq school
+  end
+end
