a5046e402a959d37d5ad21718678640c0c2e13c0
Brett <bretttanner171@gmail.com>
2024-02-02 18:56:46 +0900

    Resolve annoying RSpec warnings

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -310,4 +310,7 @@ Devise.setup do |config|
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
+
+  # Don't use deprecated secrets access method
+  config.secret_key = Rails.application.secret_key_base
 end
