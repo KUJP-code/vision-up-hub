61383999647f278b3b36cc7d9f2963eadcc05632
Brett <bretttanner171@gmail.com>
2024-02-06 17:52:57 +0900

    Update EnglishClass model & tests

--- app/models/lessons/english_class.rb
+++ app/models/lessons/english_class.rb
@@ -1,4 +1,22 @@
 # frozen_string_literal: true
 
 class EnglishClass < Lesson
+  include EnglishClassPdf, Listable
+
+  LISTABLE_ATTRIBUTES = %i[example_sentences notes vocab].freeze
+
+  attr_accessor :lesson_topic, :term, :unit
+
+  before_validation :listify_attributes, :set_topic
+
+  validates :example_sentences, :vocab, presence: true
+
+  private
+
+  # Currently we're just uploading PDFs for English classes
+  def attach_guide; end
+
+  def set_topic
+    self.topic = "Term #{term} Unit #{unit} - #{lesson_topic}"
+  end
 end
--- spec/models/lesson/english_class_spec.rb
+++ spec/models/lesson/english_class_spec.rb
@@ -6,4 +6,10 @@ RSpec.describe EnglishClass do
   it 'has a valid factory' do
     expect(build(:english_class)).to be_valid
   end
+
+  it 'sets topic from lesson_topic, term and unit' do
+    english_class = build(:english_class, lesson_topic: 'Topic', term: 1, unit: 1)
+    english_class.save
+    expect(english_class.topic).to eq('Term 1 Unit 1 - Topic')
+  end
 end
