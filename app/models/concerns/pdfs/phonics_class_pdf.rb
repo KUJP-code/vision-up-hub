8b73dbaea636125947fae26d4b47cb4a682cc908
Brett <bretttanner171@gmail.com>
2024-02-07 18:09:19 +0900

    Create PhonicsClassPdf

--- app/models/concerns/pdfs/phonics_class_pdf.rb
+++ app/models/concerns/pdfs/phonics_class_pdf.rb
@@ -10,7 +10,7 @@ module PhonicsClassPdf
     def generate_guide
       pdf = Prawn::Document.new
 
-      pdf_header(pdf, subtitle: subtype.capitalize)
+      pdf_header(pdf)
       add_unordered_lists(pdf)
       add_ordered_lists(pdf)
       pdf_links(links:, dimensions: { height: 3.cm, width: pdf.bounds.width },
@@ -21,6 +21,8 @@ module PhonicsClassPdf
   end
 
   def add_unordered_lists(pdf)
+    pdf_list(array: extra_fun, dimensions: { height: 3.cm, width: pdf.bounds.width },
+             pdf:, title: 'Extra Fun:', type: :dot)
     pdf_list(array: materials, dimensions: { height: 3.cm, width: pdf.bounds.width },
              pdf:, title: 'Materials:', type: :dot)
     pdf_list(array: notes, dimensions: { height: 3.cm, width: pdf.bounds.width },
@@ -30,6 +32,8 @@ module PhonicsClassPdf
   end
 
   def add_ordered_lists(pdf)
+    pdf_list(array: add_difficulty, dimensions: { height: 3.cm, width: pdf.bounds.width },
+             pdf:, title: 'Add Difficulty:', type: :number)
     pdf_list(array: instructions, dimensions: { height: 3.cm, width: pdf.bounds.width },
              pdf:, title: 'Instructions:', type: :number)
   end
