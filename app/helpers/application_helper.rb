1600a182e1f34374dd1892211dd5a3a1e239f105
Brett <bretttanner171@gmail.com>
2024-03-08 11:17:14 +0900

    Add #short_level helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,6 +17,23 @@ module ApplicationHelper
             class: "p-3 #{active_classes if active}"
   end
 
+  def short_level
+    case level
+    when 'land_one', 'land_two', 'land_three'
+      'Land'
+    when 'sky_one', 'sky_two', 'sky_three'
+      'Sky'
+    when 'galaxy_one', 'galaxy_two', 'galaxy_three'
+      'Galaxy'
+    when 'keep_up_one', 'keep_up_two'
+      'Keep Up'
+    when 'specialist', 'specialist_advanced'
+      'Specialist'
+    else
+      level.titleize
+    end
+  end
+
   def split_on_capitals(string)
     string.gsub(/.(?=[[:upper:]])/) { |c| "#{c} " }
   end
