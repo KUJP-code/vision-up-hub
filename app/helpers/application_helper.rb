a85e7670329252baa76806d3f3accb8ccbd145b2
jaysonpye <s3819431@student.rmit.edu.au>
2024-03-11 13:14:18 +0900

    generates links and shows alternating flag links when the page is generated

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+include LanguageHelper
   def split_on_capitals(string)
     string.gsub(/.(?=[[:upper:]])/) { |c| "#{c} " }
   end
---
+++ app/helpers/language_helper.rb
@@ -0,0 +1,10 @@
+language_helper.rb
+module LanguageHelper
+  #generates a link to toggle between English and Japanese
+  def toggle_locale_link
+    current_locale = I18n.locale
+    new_locale = (current_locale == :en) ? :ja : :en
+    link_to("Switch to #{new_locale.to_s.upcase}", url_for(locale: new_locale))
+
+  end
+end
