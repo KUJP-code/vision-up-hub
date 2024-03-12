9146db4169f6f4ea61143bcdd52ac6ad589d79e2
Jayson Pye <jaysonpye@hotmail.com>
2024-03-12 10:03:44 +0900

    remove old file, change to switching svg

--- app/helpers/language_helper.rb
+++ app/helpers/language_helper.rb
@@ -1,9 +1,10 @@
 module LanguageHelper
-  #generates a link to toggle between English and Japanese
+  # Generates a link with SVG image to toggle between English and Japanese
   def toggle_locale_link
     current_locale = I18n.locale
     new_locale = (current_locale == :en) ? :ja : :en
-    link_to("Switch to #{new_locale.to_s.upcase}", url_for(locale: new_locale))
-
+    svg_filename = (new_locale == :en) ? 'en.svg' : 'jp.svg'
+    svg_tag = image_tag(svg_filename, alt: "Switch to #{new_locale.to_s.upcase}", width: 40, height: 40)
+    link_to(svg_tag, url_for(locale: new_locale), class: 'locale-toggle-link', title: "Switch to #{new_locale.to_s.upcase}")
   end
 end
