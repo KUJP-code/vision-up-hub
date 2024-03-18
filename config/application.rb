8cafd32c1d84cef81e131753494e1c30fbd26e58
Brett <bretttanner171@gmail.com>
2024-03-18 17:15:17 +0900

    Add favicon

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,8 @@ module Materials
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
+
+    config.autoload_paths += Dir[Rails.root.join('app', 'assets', '**/')]
     # Allow nesting models and controllers in subfolders
     config.autoload_paths += Dir[Rails.root.join('app', 'controllers', '**/')]
     config.autoload_paths += Dir[Rails.root.join('app', 'models', '**/')]
