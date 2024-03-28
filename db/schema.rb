1b2573a896f2018c972d172ab412c64ab967c88e
Brett Tanner <bretttanner171@gmail.com>
2024-03-28 16:28:18 +0900

    Default new users to parent

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_28_062803) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_28_072607) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -363,7 +363,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_28_062803) do
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
-    t.string "type", default: "Teacher"
+    t.string "type", default: "Parent"
     t.bigint "organisation_id", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
