7da9f075b0a43258eae9da23ef3b07de8e260df7
Brett Tanner <bretttanner171@gmail.com>
2024-04-04 17:12:40 +0900

    Add proposal status to visiblity toggles

--- app/helpers/lesson_helper.rb
+++ app/helpers/lesson_helper.rb
@@ -1,6 +1,17 @@
 # frozen_string_literal: true
 
 module LessonHelper
+  def status_color(lesson)
+    case lesson.status
+    when 'proposed', 'changes_needed'
+      'bg-ku-orange'
+    when 'accepted'
+      'bg-green-600'
+    when 'rejected'
+      'bg-red-500'
+    end
+  end
+
   def icon_path(lesson)
     "#{lesson.type.underscore}.svg"
   end
