ec988fa84ed0c663b7f29be81ae286e9faa33de5
Brett Tanner <bretttanner171@gmail.com>
2024-04-04 13:46:03 +0900

    Handle proposal updates other than acceptance

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -7,9 +7,24 @@ class ProposalsController < ApplicationController
     @lesson = @proposal.changed_lesson
   end
 
-  def update; end
+  def update
+    return replace_lesson if proposal_params[:status] == 'accepted'
+
+    if @proposal.update(proposal_params)
+      redirect_to lesson_url(id: @proposal.changed_lesson),
+                  notice: t('update_success')
+    else
+      render :show,
+             status: :unprocessable_entity,
+             alert: t('update_failure')
+    end
+  end
 
   private
+
+  def proposal_params
+    params.require(:proposal).permit(:changed_lesson_id, :internal_notes, :status)
+  end
 
   def set_proposal
     @proposal = Lesson.find(params[:id])
