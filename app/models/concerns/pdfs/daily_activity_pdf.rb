e987f522f0ab8039afd66432d398c733e377dd7f
Brett <bretttanner171@gmail.com>
2024-04-24 12:19:43 +0900

    Add subtype to PDF

--- app/models/concerns/pdfs/daily_activity_pdf.rb
+++ app/models/concerns/pdfs/daily_activity_pdf.rb
@@ -12,7 +12,14 @@ module DailyActivityPdf
     def generate_guide
       Prawn::Document.new(margin: 0, page_size: 'A4', page_layout: :portrait) do |pdf|
         pdf.image BACKGROUND_PATH, height: 297.mm, width: 210.mm
+        draw_subtype(pdf)
       end
+    end
+  end
+
+  def draw_subtype(pdf)
+    pdf.bounding_box([27.mm, 281.mm], width: 41.mm, height: 5.mm) do
+      pdf.text subtype
     end
   end
 end
