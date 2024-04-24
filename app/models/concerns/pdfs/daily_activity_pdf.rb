adb88df63f58110f4b2c8ae1ef0f8d494fadcc79
Brett <bretttanner171@gmail.com>
2024-04-24 12:54:33 +0900

    Add title & goal box

--- app/models/concerns/pdfs/daily_activity_pdf.rb
+++ app/models/concerns/pdfs/daily_activity_pdf.rb
@@ -13,13 +13,27 @@ module DailyActivityPdf
       Prawn::Document.new(margin: 0, page_size: 'A4', page_layout: :portrait) do |pdf|
         pdf.image BACKGROUND_PATH, height: 297.mm, width: 210.mm
         draw_subtype(pdf)
+        draw_title(pdf)
+        draw_goal(pdf)
       end
     end
   end
 
   def draw_subtype(pdf)
     pdf.bounding_box([27.mm, 281.mm], width: 41.mm, height: 5.mm) do
-      pdf.text subtype
+      pdf.text subtype.titleize
+    end
+  end
+
+  def draw_title(pdf)
+    pdf.bounding_box([26.mm, 272.mm], width: 90.mm, height: 10.mm) do
+      pdf.text title, size: HEADING_SIZE
+    end
+  end
+
+  def draw_goal(pdf)
+    pdf.bounding_box([26.mm, 261.mm], width: 90.mm, height: 10.mm) do
+      pdf.text goal, size: SUBHEADING_SIZE
     end
   end
 end
