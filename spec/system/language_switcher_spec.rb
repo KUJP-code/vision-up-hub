f9ed88ff95a72c0a9f50d1bcb5f7c7afb2a9b35b
Brett <bretttanner171@gmail.com>
2024-05-27 13:16:06 +0900

    Closes #35

--- spec/system/language_switcher_spec.rb
+++ spec/system/language_switcher_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe 'language switcher' do
-  let(:user) { create(:user, :teacher) }
+  let(:user) { create(:user, :writer) }
 
   before do
     sign_in user
@@ -22,8 +22,17 @@ RSpec.describe 'language switcher' do
       visit '/'
       click_on 'Switch to EN'
       expect(I18n.locale).to eq :en
-      click_on 'Students'
+      click_on 'Lessons'
       expect(I18n.locale).to eq :en
+    end
+  end
+
+  it 'retains type query param when switching language' do
+    I18n.with_locale(:ja) do
+      visit new_lesson_path(type: 'EnglishClass')
+      click_on 'Switch to EN'
+      expect(I18n.locale).to eq :en
+      expect(page).to have_current_path new_lesson_path(locale: :en, type: 'EnglishClass')
     end
   end
 end
