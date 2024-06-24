8fb7ab9d80071bba50d99430df7ca28d5d275fff
Brett <bretttanner171@gmail.com>
2024-06-24 15:21:59 +0900

    Refactor CourseLessons JS controller

--- app/javascript/controllers/course_lessons_controller.ts
+++ app/javascript/controllers/course_lessons_controller.ts
@@ -8,16 +8,6 @@ interface planData {
 	};
 }
 
-const dayMap = {
-	sunday: 0,
-	monday: 1,
-	tuesday: 2,
-	wednesday: 3,
-	thursday: 4,
-	friday: 5,
-	saturday: 6,
-};
-
 // Connects to data-controller="course-lessons"
 export default class extends Controller {
 	static values = {
@@ -37,22 +27,44 @@ export default class extends Controller {
 
 	calculateDate() {
 		this.dateTarget.innerHTML = "";
-		const week = Number.parseInt(this.weekTarget.value) || 0;
-		const day = Number.parseInt(dayMap[this.dayTarget.value]);
+		const { week, day } = this.dateValues();
 		for (const courseId in this.plansValue) {
 			for (const orgName in this.plansValue[courseId]) {
 				const plan = this.plansValue[courseId][orgName];
 				const startDate = new Date(Date.parse(plan.startDate));
-				const lessonDate = new Date(
-					startDate.setDate(
-						startDate.getDate() + week * 7 + (day - startDate.getDay()),
-					),
-				);
+
 				this.dateTarget.insertAdjacentHTML(
 					"beforeend",
-					`<li>${orgName}: ${lessonDate.toLocaleDateString()}</li>`,
+					`<li>${orgName}: ${this.lessonDate(startDate, week, day)}</li>`,
 				);
 			}
 		}
+	}
+
+	dateValues() {
+		const dayMap = {
+			sunday: 0,
+			monday: 1,
+			tuesday: 2,
+			wednesday: 3,
+			thursday: 4,
+			friday: 5,
+			saturday: 6,
+		};
+
+		const week = Number.parseInt(this.weekTarget.value) || 0;
+		const day = Number.parseInt(dayMap[this.dayTarget.value]);
+
+		return { week, day };
+	}
+
+	lessonDate(startDate: Date, week: number, day: number) {
+		const date = new Date(
+			startDate.setDate(
+				startDate.getDate() + week * 7 + (day - startDate.getDay()),
+			),
+		);
+
+		return date.toLocaleDateString();
 	}
 }
