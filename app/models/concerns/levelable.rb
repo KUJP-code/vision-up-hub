18df3834480279aad0886f39207c77ff44b167d9
Brett Tanner <bretttanner171@gmail.com>
2024-07-16 15:55:56 +0900

    Handle missing values in order hash

--- app/models/concerns/levelable.rb
+++ app/models/concerns/levelable.rb
@@ -12,8 +12,8 @@ module Levelable
     scope :sky, -> { where(level: %i[all_levels sky_one sky_two sky_three]) }
     scope :galaxy, -> { where(level: %i[all_levels galaxy_one galaxy_two galaxy_three]) }
     scope :elementary, -> { land.or(sky).or(galaxy) }
-    scope :keep_up, -> { where(level: %i[all_levels keep_up_one keep_up_two]) }
-    scope :specialist, -> { where(level: %i[all_levels specialist specialist_advanced]) }
+    scope :keep_up, -> { where(level: %i[keep_up_one keep_up_two]) }
+    scope :specialist, -> { where(level: %i[specialist specialist_advanced]) }
 
     def elementary?
       %w[all_levels kindy keep_up_one keep_up_two specialist specialist_advanced]
