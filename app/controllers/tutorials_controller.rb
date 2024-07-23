d2146254ea1011b4480f0eb4accf7ec87e6bbfb6
Brett Tanner <bretttanner171@gmail.com>
2024-07-23 15:52:40 +0900

    Refactor video_path conversion to live on model

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class TutorialsController < ApplicationController
-  include TutorialHelper
-
   before_action :set_tutorial, only: %i[destroy show edit update]
   before_action :set_type, only: %i[new create edit update]
   before_action :set_categories, only: %i[new create edit update]
@@ -35,42 +33,50 @@ class TutorialsController < ApplicationController
     @tutorial = tutorial_class.new(tutorial_params)
 
     if @tutorial.save
-      redirect_to tutorials_path, notice: "#{params[:type]} tutorial was successfully created."
+      redirect_to tutorials_path,
+                  notice: "#{params[:type]} tutorial was successfully created."
     else
-      render :new, status: :unprocessable_entity, alert: "#{params[:type]} tutorial could not be created."
+      render :new,
+             status: :unprocessable_entity,
+             alert: "#{params[:type]} tutorial could not be created."
     end
   end
 
   def update
     if @tutorial.update(tutorial_params)
-      redirect_to tutorials_path, notice: "#{params[:type]} tutorial was successfully updated."
+      redirect_to tutorials_path,
+                  notice: "#{params[:type]} tutorial was successfully updated."
     else
-      render :edit, status: :unprocessable_entity, alert: "#{params[:type]} tutorial could not be updated."
+      render :edit,
+             status: :unprocessable_entity,
+             alert: "#{params[:type]} tutorial could not be updated."
     end
   end
 
-  # Deletes + purges file if it has one
   def destroy
     if @tutorial.destroy
-      redirect_to tutorials_path, notice: 'Tutorial was successfully deleted.'
+      redirect_to tutorials_path,
+                  notice: 'Tutorial was successfully deleted.'
     else
-      redirect_to tutorials_path, alert: 'There was an error deleting this tutorial.'
+      redirect_to tutorials_path,
+                  alert: 'There was an error deleting this tutorial.'
     end
   end
 
   private
 
-  # Permits parameters depending on type, calls helper to convert video link in the case of video here
   def tutorial_params
     case params[:type]
     when 'PDF'
       params.require(:pdf_tutorial).permit(:title, :tutorial_category_id, :file)
     when 'Video'
-      params.require(:video_tutorial).permit(:title, :tutorial_category_id, :video_path).tap do |video_params|
-        video_params[:video_path] = convert_video_link(video_params[:video_path])
-      end
+      params.require(:video_tutorial).permit(
+        :title, :tutorial_category_id, :video_path
+      )
     when 'FAQ'
-      params.require(:faq_tutorial).permit(:question, :answer, :tutorial_category_id)
+      params.require(:faq_tutorial).permit(
+        :question, :answer, :tutorial_category_id
+      )
     else
       {}
     end
--- app/models/tutorials/video_tutorial.rb
+++ app/models/tutorials/video_tutorial.rb
@@ -1,16 +1,59 @@
 # frozen_string_literal: true
 
 class VideoTutorial < ApplicationRecord
+  YOUTUBE_HOSTS = %w[youtube.com youtu.be].freeze
+  YOUTUBE_EMBED_PATH = 'https://www.youtube.com/embed/'
+
+  VIMEO_HOSTS = %w[vimeo.com].freeze
+  VIMEO_EMBED_PATH = 'https://player.vimeo.com/video/'
+
+  VALID_HOSTS = YOUTUBE_HOSTS + VIMEO_HOSTS
+
   belongs_to :tutorial_category
 
+  before_validation :convert_video_link, unless: :embeddable_link?
+
   validates :title, :video_path, presence: true
-  validate :valid_video_link
+  validate :allowed_host?
 
-  def valid_video_link
-    return if video_path.blank?
+  def allowed_host?
+    return true if VALID_HOSTS.any? { |host| video_path.include?(host) }
 
-    return if video_path.include?('youtube.com') || video_path.include?('youtu.be') || video_path.include?('vimeo.com')
+    disallowed_host_error
+  end
 
-    errors.add(:video_path, 'must be a valid YouTube or Vimeo link')
+  private
+
+  def convert_video_link
+    return disallowed_host_error unless allowed_host?
+
+    if YOUTUBE_HOSTS.any? { |host| video_path.include?(host) }
+      self.video_path = convert_youtube_link
+    elsif VIMEO_HOSTS.any? { |host| video_path.include?(host) }
+      self.video_path = "#{VIMEO_EMBED_PATH}#{video_path.split('/').last}"
+    else
+      errors.add(:video_path, 'missing conversion for given host')
+    end
+  end
+
+  def convert_youtube_link
+    video_id = if video_path.include?('youtu.be')
+                 video_path.split('/').last
+               else
+                 CGI.parse(URI.parse(video_path).query)['v'].first
+               end
+    "#{YOUTUBE_EMBED_PATH}#{video_id}"
+  rescue StandardError
+    errors.add(:video_path, "#{video_path} is not a valid URL")
+  end
+
+  def disallowed_host_error
+    errors.add(:video_path,
+               "#{video_path} does not include #{VALID_HOSTS.join(' or ')}" )
+  end
+
+  def embeddable_link?
+    video_path.include?(YOUTUBE_EMBED_PATH) ||
+      video_path.include?(VIMEO_EMBED_PATH)
   end
 end
