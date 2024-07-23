fd552f556bf965774240344a47a7affd74811529
Brett Tanner <bretttanner171@gmail.com>
2024-07-23 19:08:47 +0900

    Add the list of chonky files

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -1,10 +1,17 @@
 # frozen_string_literal: true
 
 class FilesController < ApplicationController
-  layout 'file'
+  layout :resolve_layout
 
   before_action :set_file, only: %i[show destroy]
-  after_action :verify_authorized
+  after_action :verify_authorized, except: %i[index]
+  after_action :verify_policy_scoped, only: %i[index]
+
+  def index
+    @files = policy_scope(ActiveStorage::Blob,
+                          policy_scope_class: FilePolicy::Scope)
+             .order(byte_size: :desc).limit(100)
+  end
 
   def show
     @file.update(download_count: @file.download_count + 1)
@@ -19,7 +26,17 @@ class FilesController < ApplicationController
 
   private
 
+  def resolve_layout
+    case action_name
+    when 'show'
+      'file'
+    when 'index'
+      'application'
+    end
+  end
+
   def set_file
-    @file = authorize(ActiveStorage::Blob.find(params[:id]), policy_class: FilePolicy)
+    @file = authorize(ActiveStorage::Blob.find(params[:id]),
+                      policy_class: FilePolicy)
   end
 end
