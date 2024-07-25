3158066f8a0b78eddf443a66603e9dfb1c39601f
Brett Tanner <bretttanner171@gmail.com>
2024-07-25 17:43:56 +0900

    Skip registering groups when building Docker image

--- config/initializers/flipper.rb
+++ config/initializers/flipper.rb
@@ -1,7 +1,9 @@
-Rails.application.config.after_initialize do
-  Organisation.select(:id, :name).each do |org|
-    Flipper.register(:"#{org.name.downcase.tr(' ', '_')}") do |actor, context|
-      actor.organisation_id == org.id
+unless ENV['SECRET_KEY_BASE_DUMMY']
+  Rails.application.config.after_initialize do
+    Organisation.select(:id, :name).each do |org|
+      Flipper.register(:"#{org.name.downcase.tr(' ', '_')}") do |actor, _context|
+        actor.organisation_id == org.id
+      end
     end
   end
 end
