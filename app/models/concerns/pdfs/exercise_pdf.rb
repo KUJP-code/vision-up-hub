d3f56b482bd86d2179f0179f3754e3becc6401b8
Brett <bretttanner171@gmail.com>
2024-07-31 15:55:44 +0900

    Add exercise pdf body text

--- app/models/concerns/pdfs/exercise_pdf.rb
+++ app/models/concerns/pdfs/exercise_pdf.rb
@@ -11,14 +11,15 @@ module ExercisePdf
       Prawn::Document.new(margin: 0, page_size: 'A4',
                           page_layout: :portrait) do |pdf|
         apply_defaults(pdf)
-        add_page_one(pdf)
+        factory = PdfBodyItemFactory.new(pdf)
+        add_page_one(pdf, factory)
         pdf.start_new_page
-        add_page_two(pdf)
+        add_page_two(pdf, factory)
       end
     end
   end
 
-  def add_page_one(pdf)
+  def add_page_one(pdf, factory)
     background_path =
       Rails.root.join('app/assets/pdf_backgrounds/exercise_page_one.png').to_s
     pdf.image background_path,
@@ -26,16 +27,8 @@ module ExercisePdf
               width: PAGE_WIDTH
     draw_header(pdf)
     draw_lang_goals(pdf:, y_start: 227.mm)
-    draw_body(pdf)
-    add_page_one_images(pdf:)
-  end
 
-  def add_page_two(pdf)
-    background_path =
-      Rails.root.join('app/assets/pdf_backgrounds/exercise_page_two.png').to_s
-    pdf.image background_path,
-              at: [0, PAGE_HEIGHT], height: PAGE_HEIGHT,
-              width: PAGE_WIDTH
+    draw_page_one_body(factory)
   end
 
   def draw_header(pdf)
@@ -50,5 +43,30 @@ module ExercisePdf
       pdf.text warning, color: RED, overflow: :shrink_to_fit,
                         min_font_size: 0
     end
+  end
+
+  def draw_page_one_body(factory)
+    factory.draw(text: array_to_list(materials, :number),
+                 y_pos: 200.mm, height: 16.mm)
+    factory.draw(text: array_to_list(cardio_and_stretching, :number),
+                 y_pos: 175.mm, height: 20.mm)
+    factory.draw(text: array_to_list(form_practice, :number),
+                 y_pos: 93.mm, height: 35.mm)
+  end
+
+  def add_page_two(pdf, factory)
+    background_path =
+      Rails.root.join('app/assets/pdf_backgrounds/exercise_page_two.png').to_s
+    pdf.image background_path,
+              at: [0, PAGE_HEIGHT], height: PAGE_HEIGHT,
+              width: PAGE_WIDTH
+    draw_page_two_body(factory)
+  end
+
+  def draw_page_two_body(factory)
+    factory.draw(text: array_to_list(instructions, :number),
+                 y_pos: 270.mm, height: 60.mm)
+    factory.draw(text: array_to_list(cooldown_and_recap, :number),
+                 y_pos: 100.mm, height: 20.mm)
   end
 end
