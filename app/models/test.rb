fa322a948750556519ce68dd6ff38b828f48f20c
Brett <bretttanner171@gmail.com>
2024-08-06 12:34:24 +0900

    Add course linking to test form

--- app/models/test.rb
+++ app/models/test.rb
@@ -13,6 +13,8 @@ class Test < ApplicationRecord
   validates :name, :level, :questions, :thresholds, presence: true
 
   has_many :course_tests, dependent: :destroy
+  accepts_nested_attributes_for :course_tests, allow_destroy: true,
+                                               reject_if: :all_blank
   has_many :courses, through: :course_tests
   has_many :test_results, dependent: :restrict_with_error
   has_many :students, through: :test_results
