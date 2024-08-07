cf31dacdb999b217e84d40b9442a1c8df898f9bc
Brett <bretttanner171@gmail.com>
2024-08-07 15:10:17 +0900

    Add & style org nav

--- app/controllers/lessons/lesson_calendars_controller.rb
+++ app/controllers/lessons/lesson_calendars_controller.rb
@@ -3,6 +3,7 @@
 class LessonCalendarsController < ApplicationController
   def index
     @date = Date.parse(params[:date] || Time.zone.now.beginning_of_week.to_s)
+    @orgs = policy_scope(Organisation)
     @organisation = Organisation.find(params[:org] || current_user.organisation_id)
     authorize @organisation, :show?
     @course_lessons = @organisation.week_lessons(@date).includes(:lesson)
