a493d19b534e6aabb044198b97c6bce497aed88e
Brett <bretttanner171@gmail.com>
2024-08-09 15:53:02 +0900

    Move mark_notification_read to notifiable for easier saving

--- app/models/concerns/users/notifiable.rb
+++ app/models/concerns/users/notifiable.rb
@@ -6,25 +6,32 @@ module Notifiable
   included do
     def delete_notification(index:)
       notifications.delete_at(index)
-      self.notifications = notifications.map(&:as_json)
-      save
+      mark_dirty_and_save
     end
 
     def mark_all_notifications_read
       notifications.each(&:mark_read)
-      self.notifications = notifications.map(&:as_json)
-      save
+      mark_dirty_and_save
+    end
+
+    def mark_notification_read(index:)
+      notifications[index].mark_read
+      mark_dirty_and_save
     end
 
     def notify(*new_notifications)
-      new_notifications.each { |n| notifications << n }
+      new_notifications.each { |n| notifications.prepend(n) }
       prune_read_notifications
-      self.notifications = notifications.map(&:as_json)
-      save
+      mark_dirty_and_save
     end
   end
 
   private
+
+  def mark_dirty_and_save
+    self.notifications = notifications.map(&:as_json)
+    save
+  end
 
   def prune_read_notifications
     return if notifications.size <= Notification::MAX_NOTIFICATIONS
