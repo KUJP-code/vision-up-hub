eef4aae2bf733aed1da99d5a8e35613f11ebea03
Brett Tanner <bretttanner171@gmail.com>
2024-09-03 12:15:39 +0900

    Add grid row for kindy SpecialLesson

--- app/helpers/lesson_calendar_helper.rb
+++ app/helpers/lesson_calendar_helper.rb
@@ -41,7 +41,8 @@ module LessonCalendarHelper
               lesson.short_level
             end
 
-    { 'DailyActivity' =>
+    {
+      'DailyActivity' =>
         { 'Kindy' => 'row-start-3', 'Elementary' => 'row-start-5' },
       'Exercise' =>
         { 'Kindy' => 'row-start-3', 'Elementary' => 'row-start-4' },
@@ -59,8 +60,10 @@ module LessonCalendarHelper
           'specialist' => 'row-start-[22]',
           'specialist_advanced' => 'row-start-[23]' },
       'SpecialLesson' =>
-        { 'All Levels' => 'row-start-[25]', 'Land' => 'row-start-[26]',
-          'Sky' => 'row-start-[27]', 'Galaxy' => 'row-start-[28]' } }[type][level]
+        { 'All Levels' => 'row-start-[25]', 'Kindy' => 'row-start-[26]',
+          'Land' => 'row-start-[27]', 'Sky' => 'row-start-[28]',
+          'Galaxy' => 'row-start-[29]' }
+    }[type][level]
   end
 
   def lesson_type_rows
