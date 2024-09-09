8f8c06ee46818fdc0ad378c4cbb3a97e152ae2f7
Brett <bretttanner171@gmail.com>
2024-09-09 12:29:28 +0900

    Closes #73

--- app/models/concerns/lessons/topicable.rb
+++ app/models/concerns/lessons/topicable.rb
@@ -13,6 +13,27 @@ module Topicable
       validates :term, inclusion: { in: (1..3).map(&:to_s) }
       validates :unit, inclusion: { in: (1..20).map(&:to_s) }
     end
+
+    def lesson_topic
+      return @lesson_topic if topic.blank?
+      return @lesson_topic if @lesson_topic.present?
+
+      topic[/- (.+)$/, 1]
+    end
+
+    def term
+      return @term if topic.blank?
+      return @term if @term.present?
+
+      topic[/Term (\d+)/, 1]
+    end
+
+    def unit
+      return @unit if topic.blank?
+      return @unit if @unit.present?
+
+      topic[/Unit (\d+)/, 1]
+    end
   end
 
   def set_topic
--- spec/models/lesson/english_class_spec.rb
+++ spec/models/lesson/english_class_spec.rb
@@ -9,21 +9,36 @@ RSpec.describe EnglishClass do
     expect(build(:english_class)).to be_valid
   end
 
-  context 'when setting topic' do
+  context 'when working with topic' do
+    let(:english_class) { create(:english_class, lesson_topic: 'Topic', term: 5, unit: 4) }
+
     it 'sets topic from lesson_topic, term and unit' do
-      english_class = build(:english_class, lesson_topic: 'Topic', term: 1, unit: 1)
-      english_class.save
-      expect(english_class.topic).to eq('Term 1 Unit 1 - Topic')
+      expect(english_class.topic).to eq('Term 5 Unit 4 - Topic')
     end
 
     it 'does not reset topic when released or approved' do
-      english_class = create(:english_class, lesson_topic: 'Topic',
-                                             term: '1', unit: '1')
       og_topic = english_class.topic
       english_class.update(admin_approval_id: 1, admin_approval_name: 'Admin',
                            topic: nil, term: nil, unit: nil)
       english_class.update(released: true)
       expect(english_class.reload.topic).to eq(og_topic)
+    end
+
+    it 'grabs the lesson topic from main topic col' do
+      expect(english_class.lesson_topic).to eq('Topic')
+    end
+
+    it 'grabs the term from main topic col' do
+      expect(english_class.term).to eq('5')
+    end
+
+    it 'grabs the unit from main topic col' do
+      expect(english_class.unit).to eq('4')
+    end
+
+    it 'still grabs the unit if there is a number in the lesson topic' do
+      english_class.update(lesson_topic: 'W2 Topic')
+      expect(english_class.unit).to eq('4')
     end
   end
 
