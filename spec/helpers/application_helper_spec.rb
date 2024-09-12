648304b1ea14b9b67bd58a07c9076a763bd64b41
Brett Tanner <bretttanner171@gmail.com>
2024-09-12 17:46:57 +0900

    Default monthly materials to current week

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -4,11 +4,7 @@ require 'rails_helper'
 
 RSpec.describe ApplicationHelper do
   context 'when setting theme from organisation' do
-    let(:user) { build(:user, :org_admin, organisation_id: 2) }
-
-    it 'returns a string in the form org_n' do
-      expect(org_theme(user)).to eq('org_2')
-    end
+    let(:user) { build(:user, :org_admin) }
 
     it "returns 'base' if not in list of themed orgs" do
       user.organisation_id = 999 # congrats if this breaks
