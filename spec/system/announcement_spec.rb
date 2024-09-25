ddeabccad5d1afe80063efdb569ad25807c1098b
jaysonpye <jaysonpye@hotmail.com>
2024-09-25 10:49:21 +0900

    finalize announcement system spec

--- spec/system/announcement_spec.rb
+++ spec/system/announcement_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-RSpec.describe 'Creating and viewing an announcement', type: :system do
+RSpec.describe 'Creating and viewing an announcement' do
   let(:admin) { create(:user, :org_admin) }
   let(:teacher) { create(:user, :teacher) }
 
@@ -13,21 +13,21 @@ RSpec.describe 'Creating and viewing an announcement', type: :system do
   it 'allows an admin to create an announcement' do
     visit new_announcement_path
 
-    fill_in 'announcement_message', with: 'Important announcement for students!'
+    fill_in 'announcement_message', with: 'Test announcement'
     fill_in 'announcement_valid_from', with: Time.Zone.today
     fill_in 'announcement_valid_until', with: Time.Zone.today + 7.days
-    fill_in 'announcement_link', with: '/students'
+    fill_in 'announcement_link', with: students_path
 
-    click_button 'Create Announcement'
+    click_button 'input[type=submit]'
 
-    expect(page).to have_content('Announcement created successfully')
+    expect(page).to have_content('Test announcement')
 
     sign_out admin
     sign_in teacher
 
-    visit teacher_announcements_path
+    visit root_path
 
-    expect(page).to have_content('Important announcement for students!')
-    expect(page).to have_link('More info', href: '/students')
+    expect(page).to have_content('Test announcement')
+    expect(find_by_id('announcements')).to have_link(href: students_path)
   end
 end
