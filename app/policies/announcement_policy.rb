e07a68facfb0ccc8614afc43ee9078a81d8880f9
jaysonpye <jaysonpye@hotmail.com>
2024-09-25 14:10:10 +0900

    add announcement policy and spec

---
+++ app/policies/announcement_policy.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+class AnnouncementPolicy < ApplicationPolicy
+  def show?
+    user.is?('Admin') || announcement_org_admin?
+  end
+
+  def new?
+    user.is?('Admin') || announcement_org_admin?
+  end
+
+  def edit?
+    user.is?('Admin') || announcement_org_admin?
+  end
+
+  def update?
+    user.is?('Admin') || announcement_org_admin?
+  end
+
+  def create?
+    user.is?('Admin') || announcement_org_admin?
+  end
+
+  def destroy?
+    user.is?('Admin') || announcement_org_admin?
+  end
+
+  class Scope < Scope
+    def resolve
+      case user.type
+      when 'Admin'
+        scope.all
+      when 'OrgAdmin'
+        scope.where(organisation_id: [user.organisation_id, nil])
+      when 'Writer', 'Sales', 'SchoolManager', 'Teacher', 'Parent'
+        scope.where(organisation_id: [user.organisation_id, nil], role: [user.type, nil])
+      end
+    end
+  end
+
+  private
+
+  def announcement_org_admin?
+    user.is?('OrgAdmin') && user.organisation_id == record.organisation_id
+  end
+end
---
+++ spec/policies/announcement_policy_spec.rb
@@ -0,0 +1,124 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe AnnouncementPolicy do
+  subject(:announcement) { described_class.new(user, record) }
+
+  let(:record) { build(:announcement) }
+
+  context 'when admin' do
+    let(:user) { build(:user, :admin) }
+
+    it_behaves_like 'authorized user'
+
+    it 'scopes to all announcements' do
+      record.save
+      expect(Pundit.policy_scope!(user, Announcement)).to contain_exactly(record)
+    end
+  end
+
+  context 'when writer' do
+    let(:user) { build(:user, :writer) }
+
+    it_behaves_like 'unauthorized user'
+
+    it 'scopes to targetted announcements' do
+      create(:announcement, organisation: create(:organisation), role: 'Sales') # unseen
+      role_announcement = create(:announcement, role: 'Writer', message: 'Writer announcement')
+      org = create(:organisation, name: 'KidsUP')
+      user.save
+      user.update(organisation_id: org.id)
+      org_announcement = create(:announcement, message: 'Org Announcement', organisation: user.organisation)
+      expect(Pundit.policy_scope!(user, Announcement)).to contain_exactly(role_announcement, org_announcement)
+    end
+  end
+
+  context 'when sales' do
+    let(:user) { build(:user, :sales) }
+
+    it_behaves_like 'unauthorized user'
+
+    it 'scopes to targetted announcements' do
+      create(:announcement, organisation: create(:organisation), role: 'Writer') # unseen
+      role_announcement = create(:announcement, role: 'Sales')
+      org = create(:organisation, name: 'KidsUP')
+      user.save
+      user.update(organisation_id: org.id)
+      org_announcement = create(:announcement, organisation: user.organisation)
+      expect(Pundit.policy_scope!(user, Announcement)).to contain_exactly(role_announcement, org_announcement)
+    end
+  end
+
+  context 'when org_admin' do
+    let(:user) { create(:user, :org_admin) }
+
+    context 'when different org_admin' do
+      it_behaves_like 'unauthorized user'
+    end
+
+    context 'when same org_admin' do
+      before do
+        record.save
+        record.update(organisation_id: user.organisation_id)
+      end
+
+      it_behaves_like 'authorized user'
+    end
+
+    it 'scopes to targetted announcements' do
+      create(:announcement, organisation: create(:organisation)) # unseen because its a different organisation
+      org_announcement = create(:announcement, organisation: user.organisation)
+      generic_announcement = create(:announcement)
+      expect(Pundit.policy_scope!(user, Announcement)).to contain_exactly(generic_announcement, org_announcement)
+    end
+  end
+
+  context 'when SchoolManager' do
+    let(:user) { build(:user, :school_manager) }
+
+    it_behaves_like 'unauthorized user'
+
+    it 'scopes to targetted announcements' do
+      create(:announcement, organisation: create(:organisation), role: 'Sales') # unseen
+      role_announcement = create(:announcement, role: 'SchoolManager', message: 'School Manager announcement')
+      org = create(:organisation)
+      user.save
+      user.update(organisation_id: org.id)
+      org_announcement = create(:announcement, message: 'Org Announcement', organisation: user.organisation)
+      expect(Pundit.policy_scope!(user, Announcement)).to contain_exactly(role_announcement, org_announcement)
+    end
+  end
+
+  context 'when Teacher' do
+    let(:user) { build(:user, :teacher) }
+
+    it_behaves_like 'unauthorized user'
+
+    it 'scopes to targetted announcements' do
+      create(:announcement, organisation: create(:organisation), role: 'Sales') # unseen
+      role_announcement = create(:announcement, role: 'Teacher', message: 'Teacher announcement')
+      org = create(:organisation)
+      user.save
+      user.update(organisation_id: org.id)
+      org_announcement = create(:announcement, message: 'Org Announcement', organisation: user.organisation)
+      expect(Pundit.policy_scope!(user, Announcement)).to contain_exactly(role_announcement, org_announcement)
+    end
+  end
+
+  context 'when Parent' do
+    let(:user) { build(:user, :parent) }
+
+    it_behaves_like 'unauthorized user'
+
+    it 'scopes to targetted announcements' do
+      create(:announcement, organisation: create(:organisation), role: 'Sales') # unseen
+      role_announcement = create(:announcement, role: 'Parent', message: 'Parent announcement')
+      org = create(:organisation)
+      user.save
+      user.update(organisation_id: org.id)
+      org_announcement = create(:announcement, message: 'Org Announcement', organisation: user.organisation)
+      expect(Pundit.policy_scope!(user, Announcement)).to contain_exactly(role_announcement, org_announcement)
+    end
+  end
+end
