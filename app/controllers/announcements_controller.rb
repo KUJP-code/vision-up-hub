d09f0c0950d86941abf3e921d81512afc86c4a45
jaysonpye <jaysonpye@hotmail.com>
2024-09-27 18:14:19 +0900

    add destroy/update

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class AnnouncementsController < ApplicationController
+  before_action :set_announcement, only: %i[destroy edit update]
   after_action :verify_authorized, except: :index
   after_action :verify_policy_scoped, only: :index
 
@@ -8,17 +9,17 @@ class AnnouncementsController < ApplicationController
     @announcements = policy_scope(Announcement).order(start_date: :desc)
   end
 
-  def show; end
-
   def new
     @announcement = authorize Announcement.new(organisation_id: current_user.organisation_id)
     set_form_data
   end
 
-  def edit; end
+  def edit
+    set_form_data
+  end
 
   def create
-    @announcement = authorize Announcement.new(announcement_params)
+    @announcement = authorize Announcement.new(announcement_param)
     if @announcement.save
       redirect_to announcements_url, notice: t('success')
     else
@@ -28,14 +29,31 @@ class AnnouncementsController < ApplicationController
     end
   end
 
-  def update; end
+  def update
+    if @announcement.update(announcement_params)
+      redirect_to announcements_url, notice: t('success')
+    else
+      set_form_data
+      render :edit, alert: t('failure')
+    end
+  end
 
-  def destroy; end
+  def destroy
+    if @announcement.destroy
+      redirect_to announcements_url, notice: t('success')
+    else
+      redirect_to announcements_url, alert: t('failure')
+    end
+  end
 
   private
 
   def announcement_params
     params.require(:announcement).permit(:message, :link, :finish_date, :start_date, :organisation_id, :role)
+  end
+
+  def set_announcement
+    @announcement = authorize Announcement.find(params[:id])
   end
 
   def set_form_data
