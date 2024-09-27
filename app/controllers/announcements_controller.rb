c0ff1fa12e8c84916b9b2d12684e983b5627f66d
jaysonpye <jaysonpye@hotmail.com>
2024-09-27 15:42:03 +0900

    add form related views and index

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -4,17 +4,41 @@ class AnnouncementsController < ApplicationController
   after_action :verify_authorized, except: :index
   after_action :verify_policy_scoped, only: :index
 
-  def index; end
+  def index
+    @announcements = policy_scope(Announcement)
+  end
 
   def show; end
 
-  def new; end
+  def new
+    @announcement = authorize Announcement.new(organisation_id: current_user.organisation_id)
+    set_form_data
+  end
 
   def edit; end
 
-  def create; end
+  def create
+    @announcement = authorize Announcement.new(announcement_params)
+    if @announcement.save
+      redirect_to announcements_url, notice: t('success')
+    else
+      set_form_data
+      render :new, alert: t('failure')
+
+    end
+  end
 
   def update; end
 
   def destroy; end
+
+  private
+
+  def announcement_params
+    params.require(:announcement).permit(:message, :link, :finish_date, :start_date, :organisation_id, :role)
+  end
+
+  def set_form_data
+    @organisations = policy_scope(Organisation)
+  end
 end
