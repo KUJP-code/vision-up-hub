cce020730d897f9b9c73114593d7323eebf7bf44
Brett Tanner <bretttanner171@gmail.com>
2024-10-01 18:46:52 +0900

    Allow www versions of promo site in CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://mike.vision-up.biz', 'https://vision-up.biz'
+    origins %w[https://mike.vision-up.biz https://www.mike.vision-up.biz https://vision-up.biz https://www.vision-up.biz]
     resource '/inquiries', headers: :any, methods: %i[post]
   end
 end
