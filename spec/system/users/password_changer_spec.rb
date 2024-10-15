2e2f2b3b97aea70d8052335af812edf49987153f
jaysonpye <jaysonpye@hotmail.com>
2024-10-15 16:47:46 +0900

    Deploy 1.0.8-add-announcements

---
+++ spec/system/users/password_changer_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Change passwords for users'
+let(:admin) { create(:user, :admin) }
+let(:user) { create(:user, :teacher) }
+
+before do
+  sign_in admin
+end
+
+it 'allows an admin to change another users password' do
+  visit password_change_path
+  fill_in 'users email'
+  fill_in 'new password', with: 'testpassword'
+  fill_in 'confirm new password', with: 'testpassword'
+  click_on 'commit'
+end
