c99f19d34347b45cf3ce6b1cfff11220cc0dd156
jaysonpye <jaysonpye@hotmail.com>
2024-10-15 16:37:43 +0900

    add headers to applicationmailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: 'system@vision-up.app'
+  # Unsubscribe stuff doesn't make sense here, it is added to try to avoid spam
+  default from: 'support@vision-up.app',
+          'List-Unsubscribe-Post' => 'List-Unsubscribe=One-Click',
+          'List-Unsubscribe' => 'https://vision-up.app'
   layout 'mailer'
 end
