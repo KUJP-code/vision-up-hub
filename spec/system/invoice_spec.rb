b0c37456291a5edeef1b6b08ad8d96a979b203fe
jaysonpye <jaysonpye@hotmail.com>
2024-12-05 14:00:02 +0900

    edit spec

--- spec/system/invoice_spec.rb
+++ spec/system/invoice_spec.rb
@@ -7,38 +7,32 @@ RSpec.describe 'Creating an Invoice' do
   let!(:organisation) { create(:organisation, name: 'Test Organisation', number_of_kids: 50) }
 
   before do
-    sign_in admin # Assuming Devise or similar is being used
+    sign_in admin
   end
 
   it 'allows an admin to create an invoice' do
     visit invoices_path
 
-    # Click the button to navigate to the new invoice form
     click_link 'Create New Invoice'
 
-    # Fill in the form
     select 'Test Organisation', from: 'invoice_organisation_id'
     select 'Default', from: 'invoice_payment_option'
     fill_in 'invoice_issued_at', with: Date.today
 
-    # Submit the form
     click_button 'Create Invoice'
 
-    # Check for success messages or resulting state
     expect(page).to have_content('Invoice was successfully created')
     expect(page).to have_content('Test Organisation')
-    expect(page).to have_content('50') # Number of kids
+    expect(page).to have_content('50')
     expect(page).to have_content('Default')
-    expect(page).to have_content((50 * PricingCalculatable::PAYMENT_OPTIONS[:default]).to_s) # Subtotal
+    expect(page).to have_content((50 * PricingCalculatable::PAYMENT_OPTIONS[:default]).to_s)
   end
 
   it 'shows validation errors if form is submitted without required fields' do
     visit new_invoice_path
 
-    # Submit form without filling it in
     click_button 'Create Invoice'
 
-    # Check for error messages
     expect(page).to have_content("Organisation can't be blank")
     expect(page).to have_content("Payment option can't be blank")
     expect(page).to have_content("Issued at can't be blank")
