12ba0cb3764eed33366e8f37e55365dd9b7fc063
jaysonpye <jaysonpye@hotmail.com>
2024-12-11 12:16:20 +0900

    add events system specs

---
+++ spec/system/lessons/party_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'creating a Party' do
+  let!(:org) { create(:organisation, name: 'KidsUP') }
+  let(:dummy_file_path) { Rails.root.join('spec/Brett_Tanner_Resume.pdf') }
+
+  before do
+    user = org.users.create(attributes_for(:user, :writer))
+    sign_in user
+  end
+
+  it 'can create an Party' do
+    visit lessons_path
+    find_by_id('create_lesson').click
+    click_link 'create_party'
+    within '#party_form' do
+      fill_in 'party_title', with: 'Test Party'
+      attach_file('Materials List', dummy_file_path)
+      attach_file('Guides', dummy_file_path, dummy_file_path)
+      attach_file('Promotion', dummy_file_path)
+      fill_in 'Party Date', with: Time.Zone.Today
+      fill_in 'Show From', with: Time.Zone.Today
+      fill_in 'Show Until', with: Time.zone.Today + 7.days
+      click_button 'commit'
+
+      expect(page).to have_content('Test Party')
+      expect(page).to have_content('img.guide_image')
+    end
+  end
+end
--- spec/system/lessons/seasonal_activity_spec.rb
+++ spec/system/lessons/seasonal_activity_spec.rb
@@ -2,8 +2,9 @@
 
 require 'rails_helper'
 
-RSpec.describe 'creating a seasonal activit' do
+RSpec.describe 'creating a seasonal activity' do
   let!(:org) { create(:organisation, name: 'KidsUP') }
+  let(:dummy_file_path) { Rails.root.join('spec/Brett_Tanner_Resume.pdf') }
 
   before do
     user = org.users.create(attributes_for(:user, :writer))
@@ -16,6 +17,17 @@ RSpec.describe 'creating a seasonal activit' do
     click_link 'create_seasonal_activity'
     within '#seasonal_activity_form' do
       fill_in 'seasonal_activity_title', with: 'Test Seasonal Activity'
+      attach_file('Ele English Class', dummy_file_path)
+      attach_file('Kindy English Class', dummy_file_path)
+      attach_file('Guides', dummy_file_path, dummy_file_path)
+      attach_file('Scrapbook', dummy_file_path)
+      fill_in 'Event Date', with: Time.Zone.Today
+      fill_in 'Show From', with: Time.Zone.Today
+      fill_in 'Show until', with: Time.zone.Today + 7.days
+      click_button 'commit'
+
+      expect(page).to have_content('Test Seasonal')
+      expect(page).to have_content('img.guide_image')
     end
   end
 end
