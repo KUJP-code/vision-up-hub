7d51deda80aed81341fc9f6a565973ce0c06e2db
jaysonpye <jaysonpye@hotmail.com>
2024-12-22 17:09:04 +0900

    add front end view

---
+++ app/controllers/lessons/teacher_events_controller.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+class TeacherEventsController < ApplicationController
+  after_action :verify_authorized, only: %i[show]
+  SUPPORTED_TYPES = %w[seasonalactivity parties events].freeze
+  def index
+    index_vars
+    respond_to do |format|
+      format.html
+      format.turbo_stream
+    end
+  end
+
+  def show
+    set_date_type_teacher
+    @lesson = Lesson.find(params[:id])
+    authorize @lesson
+  end
+
+  private
+
+  def index_vars
+    set_date_type_teacher
+
+    @supported_types = SUPPORTED_TYPES
+    @announcements = Pundit.policy_scope!(@teacher, Announcement)
+    @lessons = Lesson.where('show_from <=? AND show_until >= ?', @date, @date).order(:event_date)
+    logger.debug "Fetched Lessons: #{@lessons.pluck(:id, :type, :title)}"
+  end
+
+  def set_date_type_teacher
+    @teacher = current_user
+    @date = params[:date] ? Date.parse(params[:date]) : Time.zone.today
+  end
+
+  def validated_type(type_param)
+    if SUPPORTED_TYPES.none?(type_param)
+      redirect_back fallback_location: root_path, alert: "Invalid lesson type: #{type_param}"
+    else
+      type_param
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -45,7 +45,7 @@ Rails.application.routes.draw do
         resources :support_messages, only: %i[create]
       end
       resources :teacher_lessons, only: %i[index show]
-      resources :teacher_event_lessons, only: %i[index show]
+      resources :teacher_events, only: %i[index show]
       resources :teacher_resources, only: %i[index]
       resources :tests, except: %i[show] do
         resources :test_results, only: %i[create index update]
