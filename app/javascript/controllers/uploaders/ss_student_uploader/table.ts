3705fd9b967c8d7f1c2abc5b9859dec0e41d691b
jaysonpye <jaysonpye@hotmail.com>
2025-01-27 13:41:31 +0900

    add js for ss_uploads

---
+++ app/javascript/controllers/uploaders/ss_student_uploader/table.ts
@@ -0,0 +1,20 @@
+import { tableHeader } from "../table.ts";
+
+export function newSsStudentUploadTable(headers: string[]) {
+  let headerString = headers
+    .map((header) => tableHeader(header, headers.indexOf(header)))
+    .join("");
+  headerString += `<th class="thead thead-e bg-secondary-50">status</th>`;
+
+  return `
+  <table class="w-full text-center">
+      <thead>
+          <tr>
+              ${headerString}
+          </tr>
+      </thead>
+      <tbody id="student-table">
+      </tbody>
+  </table>
+  `;
+}
