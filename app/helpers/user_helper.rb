791a7fc7df0c762fb2f862c78fe8eac891db6990
jaysonpye <jaysonpye@hotmail.com>
2025-05-26 12:13:11 +0900

    fallback button fix when no school

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -7,22 +7,29 @@ module UserHelper
   end
 
   def todays_lessons_link
+    return fallback_lessons_link unless current_user.respond_to?(:schools)
+
+    school_ids = current_user.schools.select(:id)
+
+    return fallback_lessons_link if school_ids.blank?
+
     first_teacher = Teacher
       .joins(:schools)
-      .where(schools: { id: current_user.schools.select(:id) })
+      .where(schools: { id: school_ids })
       .first
 
+    return fallback_lessons_link unless first_teacher
 
-    if first_teacher
-      link_to organisation_teacher_path(id: first_teacher.id, organisation_id: current_user.organisation.id),
-              class: 'btn btn-outline-white w-full flex gap-2' do
-        inline_svg_tag('todays_lesson.svg', class: 'w-10 h-10') + t('.todays_lessons')
-      end
-    else
-      # Fallback btn
-      content_tag(:span, class: 'btn btn-outline-white w-full flex gap-2 cursor-not-allowed opacity-50') do
-        inline_svg_tag('todays_lesson.svg', class: 'w-10 h-10') + ' Todays Lessons'.html_safe
-      end
+    link_to organisation_teacher_path(id: first_teacher.id, organisation_id: current_user.organisation_id),
+            class: 'btn btn-outline-white w-full flex gap-2' do
+      inline_svg_tag('todays_lesson.svg', class: 'w-10 h-10') + t('.todays_lessons')
     end
   end
+
+  def fallback_lessons_link
+    content_tag(:span, class: 'btn btn-outline-white w-full flex gap-2 cursor-not-allowed opacity-50') do
+      inline_svg_tag('todays_lesson.svg', class: 'w-10 h-10') + ' Todays Lessons'.html_safe
+    end
+  end
+
 end
