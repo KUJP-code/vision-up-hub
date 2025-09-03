db784d61fa81522cb63245cdffec89c4f97a2989
jaysonpye <jaysonpye@hotmail.com>
2025-09-03 14:27:43 +0900

    add indexes to migration

--- db/migrate/20250725013946_create_devices.rb
+++ db/migrate/20250725013946_create_devices.rb
@@ -2,16 +2,18 @@ class CreateDevices < ActiveRecord::Migration[7.1]
   def change
     create_table :devices do |t|
       t.references :user, null: false, foreign_key: true
-      t.string :token
+      t.string :token, null: false
       t.string :user_agent
       t.string :platform
       t.string :ip_address
       t.integer :status, default: 0, null: false
 
       t.timestamps
-
-      add_index :devices, [:user_id, :token], unique: true
-
     end
+
+    add_index :devices, [:user_id, :token], unique: true
+    add_index :devices, :status
+    add_index :devices, :token
+    add_index :devices, :user_id
   end
 end
