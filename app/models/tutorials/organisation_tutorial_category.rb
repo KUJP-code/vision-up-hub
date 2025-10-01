f45396dfba9788adacf85e0e7b85af050c7fe01f
jaysonpye <jaysonpye@hotmail.com>
2025-10-01 10:20:07 +0900

    org_categories views and controller

--- app/models/tutorials/organisation_tutorial_category.rb
+++ app/models/tutorials/organisation_tutorial_category.rb
@@ -3,4 +3,8 @@
 class OrganisationTutorialCategory < ApplicationRecord
   belongs_to :organisation
   belongs_to :tutorial_category
+
+  validates :organisation_id,
+            uniqueness: { scope: :tutorial_category_id,
+                          message: 'is already attached to this category' }
 end
