e8ac095c8048bfc2a7aaf4e1d6a56709dd98478e
jaysonpye <jaysonpye@hotmail.com>
2025-10-01 10:38:37 +0900

    add tests

---
+++ spec/factories/organisation_tutorial_categories.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :organisation_tutorial_category do
+    organisation
+    tutorial_category
+  end
+end
--- spec/policies/tutorial_category_policy_spec.rb
+++ spec/policies/tutorial_category_policy_spec.rb
@@ -5,7 +5,12 @@ require 'rails_helper'
 RSpec.describe TutorialCategoryPolicy do
   subject(:policy) { described_class.new(user, record) }
 
-  let(:record) { build(:tutorial_category) }
+  let(:organisation) { create(:organisation) }
+  let(:record) do
+    create(:tutorial_category).tap do |cat|
+      create(:organisation_tutorial_category, organisation:, tutorial_category: cat)
+    end
+  end
 
   context 'when admin' do
     let(:user) { build(:user, :admin) }
@@ -14,7 +19,7 @@ RSpec.describe TutorialCategoryPolicy do
   end
 
   context 'when writer' do
-    let(:user) { build(:user, :writer) }
+    let(:user) { build(:user, :writer, organisation:) }
 
     it_behaves_like 'authorized user for viewing'
   end
@@ -26,25 +31,25 @@ RSpec.describe TutorialCategoryPolicy do
   end
 
   context 'when org admin' do
-    let(:user) { build(:user, :org_admin) }
+    let(:user) { build(:user, :org_admin, organisation:) }
 
     it_behaves_like 'authorized user for viewing'
   end
 
   context 'when school manager' do
-    let(:user) { build(:user, :school_manager) }
+    let(:user) { build(:user, :school_manager, organisation:) }
 
     it_behaves_like 'authorized user for viewing'
   end
 
   context 'when teacher' do
-    let(:user) { build(:user, :teacher) }
+    let(:user) { build(:user, :teacher, organisation:) }
 
     it_behaves_like 'authorized user for viewing'
   end
 
   context 'when parent' do
-    let(:user) { build(:user, :parent) }
+    let(:user) { build(:user, :parent, organisation:) }
 
     it_behaves_like 'authorized user for viewing'
   end
