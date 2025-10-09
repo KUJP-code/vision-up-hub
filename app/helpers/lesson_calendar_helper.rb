191a2245fd04b0e702dfd1cd0bdd41d361e1f3f0
jaysonpye <jaysonpye@hotmail.com>
2025-10-09 11:08:35 +0900

    fix edge bugs by throwing types to string

--- app/helpers/lesson_calendar_helper.rb
+++ app/helpers/lesson_calendar_helper.rb
@@ -2,8 +2,6 @@
 
 module LessonCalendarHelper
   def calendar_date(date, course_lesson)
-    # date is always a Monday to be start of week
-    # so need to get the date offset by 2 from cl day
     date + (CourseLesson.days[course_lesson.day] - 2).days
   end
 
@@ -28,70 +26,104 @@ module LessonCalendarHelper
   end
 
   def lesson_row(lesson)
-    type = case lesson.type
-           when 'KindyPhonic'
-             'PhonicsClass'
-           else
-             lesson.type
-           end
+    type_key =
+      case lesson&.type
+      when 'KindyPhonic' then 'PhonicsClass'
+      else lesson&.type.to_s
+      end
 
-    level = if lesson.short_level == 'Specialist'
-              lesson.level
-            else
-              lesson.short_level
-            end
+    level_key =
+      if lesson&.short_level == 'Specialist'
+        lesson&.level.to_s
+      else
+        lesson&.short_level.to_s
+      end
 
-    {
-      'SpecialLesson' =>
-      { 'All Levels' => 'row-start-3', 'Kindy' => 'row-start-4',
-        'Land' => 'row-start-5', 'Sky' => 'row-start-6',
-        'Galaxy' => 'row-start-7' },
-      'DailyActivity' =>
-          { 'Kindy' => 'row-start-9', 'Elementary' => 'row-start-11' },
-      'Exercise' =>
-          { 'Kindy' => 'row-start-9', 'Elementary' => 'row-start-10' },
-      'PhonicsClass' =>
-          { 'Kindy' => 'row-start-13', 'Land' => 'row-start-[14]',
-            'Sky' => 'row-start-[15]', 'Galaxy' => 'row-start-[16]' },
-      'EnglishClass' =>
-          { 'Kindy' => 'row-start-[18]', 'Land' => 'row-start-[19]',
-            'Sky' => 'row-start-[20]', 'Galaxy' => 'row-start-[21]' },
-      'StandShowSpeak' =>
-          { 'Kindy' => 'row-start-[23]', 'Land' => 'row-start-[24]', 'Sky' => 'row-start-[25]',
-            'Galaxy' => 'row-start-[26]' },
-      'EveningClass' =>
-          { 'Keep Up' => 'row-start-[28]',
-            'specialist' => 'row-start-[29]',
-            'specialist_advanced' => 'row-start-[30]' },
-      'ParentsReport' => { 'All Levels' => 'row-start-[32]' }
-    }[type][level]
+    rows_by_type = {
+      'SpecialLesson' => {
+        'All Levels' => 'row-start-3',
+        'Kindy' => 'row-start-4',
+        'Land' => 'row-start-5',
+        'Sky' => 'row-start-6',
+        'Galaxy' => 'row-start-7'
+      },
+      'DailyActivity' => {
+        'Kindy' => 'row-start-9',
+        'Elementary' => 'row-start-11'
+      },
+      'Exercise' => {
+        'Kindy' => 'row-start-9',
+        'Elementary' => 'row-start-10'
+      },
+      'PhonicsClass' => {
+        'Kindy' => 'row-start-13',
+        'Land' => 'row-start-[14]',
+        'Sky' => 'row-start-[15]',
+        'Galaxy' => 'row-start-[16]'
+      },
+      'EnglishClass' => {
+        'Kindy' => 'row-start-[18]',
+        'Land' => 'row-start-[19]',
+        'Sky' => 'row-start-[20]',
+        'Galaxy' => 'row-start-[21]'
+      },
+      'StandShowSpeak' => {
+        'Kindy' => 'row-start-[23]',
+        'Land' => 'row-start-[24]',
+        'Sky' => 'row-start-[25]',
+        'Galaxy' => 'row-start-[26]'
+      },
+      'EveningClass' => {
+        'Keep Up' => 'row-start-[28]',
+        'specialist' => 'row-start-[29]',
+        'specialist_advanced' => 'row-start-[30]'
+      },
+      'ParentsReport' => {
+        'All Levels' => 'row-start-[32]'
+      }
+    }
+
+    row = rows_by_type.dig(type_key, level_key)
+
+    unless row
+      Rails.logger.warn(
+        "lesson_row: missing map for type=#{type_key.inspect} level=#{level_key.inspect} " \
+        "(lesson_id=#{lesson&.id}, title=#{lesson&.try(:title)})"
+      )
+      row = 'row-start-[33]'
+    end
+
+    row
   end
 
   def lesson_separator_rows
-    { 'DailyActivity' => 'row-start-6',
+    {
+      'DailyActivity' => 'row-start-6',
       'PhonicsClass' => 'row-start-12',
       'EnglishClass' => 'row-start-[17]',
       'StandShowSpeak' => 'row-start-[22]',
       'EveningClass' => 'row-start-[27]',
-      'ParentsReport' => 'row-start-[31]' }
+      'ParentsReport' => 'row-start-[31]'
+    }
   end
 
   def lesson_type_rows
-    { 'SpecialLesson' => 'row-start-3 row-span-3',
+    {
+      'SpecialLesson' => 'row-start-3 row-span-3',
       'DailyActivity' => 'row-start-9 row-span-3',
       'PhonicsClass' => 'row-start-13 row-span-4',
       'EnglishClass' => 'row-start-[18] row-span-4',
       'StandShowSpeak' => 'row-start-[23] row-span-4',
       'EveningClass' => 'row-start-[28] row-span-3',
-      'ParentsReport' => 'row-start-[32] row-span-2' }
+      'ParentsReport' => 'row-start-[32] row-span-2'
+    }
   end
 
   def calendar_level_dots(level)
     base_dot_classes = 'absolute rounded-full w-3 h-3 top-2'
     return all_levels_dot(base_dot_classes) if ['All Levels', 'Elementary'].include?(level)
 
-    content_tag(:div, '',
-                class: "#{base_dot_classes} right-2 #{calendar_dot_bg(level)}")
+    content_tag(:div, '', class: "#{base_dot_classes} right-2 #{calendar_dot_bg(level)}")
   end
 
   def calendar_dot_bg(level)
