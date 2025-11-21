85f8266e3988fb9a456ae19c7a18b33de85cc37b
jaysonpye <jaysonpye@hotmail.com>
2025-11-21 13:35:30 +0900

    make cards/headings dependent on if resources are even available or not

--- app/controllers/teacher_resources_controller.rb
+++ app/controllers/teacher_resources_controller.rb
@@ -4,23 +4,40 @@ class TeacherResourcesController < ApplicationController
   after_action :verify_policy_scoped, only: :index
 
   def index
-    @lesson_category = set_lesson_category
-    @category_resources = policy_scope(CategoryResource)
-                          .with_attached_resource
-                          .send(@lesson_category)
+    base_scope = policy_scope(CategoryResource).with_attached_resource
+
+    @allowed_categories = allowed_categories_for(current_user, base_scope)
+    @lesson_category    = set_lesson_category
+    return if performed? # in case we redirected
+
+    @category_resources = base_scope.public_send(@lesson_category)
   end
 
   private
 
-  def set_lesson_category
-    allowed_categories = CategoryResource.lesson_categories.keys
-    unless Flipper.enabled?(:afterschool_extras, current_user)
-      allowed_categories -= CategoryResource::AFTERSCHOOL_EXTRAS
+  # All Flipper + "only categories with records" logic
+  def allowed_categories_for(user, base_scope)
+    categories = CategoryResource.lesson_categories.keys
+
+    categories -= CategoryResource::AFTERSCHOOL_EXTRAS unless Flipper.enabled?(:afterschool_extras, user)
+
+    unless Flipper.enabled?(:keep_up, user) ||
+           Flipper.enabled?(:specialist, user)
+      categories -= %w[evening_class]
     end
-    category = params[:category]
-    unless allowed_categories.include?(category)
-      redirect_to teacher_resources_url(category: allowed_categories.first),
+
+    # 🔑 Only keep categories that actually have records in the scoped data
+    categories.select { |cat| base_scope.public_send(cat).exists? }
+  end
+
+  def set_lesson_category
+    category = params[:category].to_s
+    category = @allowed_categories.first if category.blank?
+
+    unless @allowed_categories.include?(category)
+      redirect_to teacher_resources_url(category: @allowed_categories.first),
                   alert: t('not_authorized')
+      return
     end
 
     category
