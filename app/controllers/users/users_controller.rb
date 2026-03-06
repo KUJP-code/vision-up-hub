f163fd2b8d126a14734a4345315d8e618eafc2be
jaysonpye <jaysonpye@hotmail.com>
2026-03-06 16:45:20 +0900

    add lesson fallback for user delete

--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -24,11 +24,17 @@ class UsersController < ApplicationController
   def update; end
 
   def destroy
-    if @user.destroy
-      redirect_to users_path, notice: "#{@user.name} successfully deleted."
-    else
-      redirect_to users_path, alert: "Couldn't delete user"
+    fallback_admin = fallback_admin_for(@user)
+    return redirect_to users_path, alert: 'Cannot delete user without an admin fallback' if fallback_admin.nil?
+
+    ActiveRecord::Base.transaction do
+      reassign_lessons_to_fallback(@user, fallback_admin)
+      @user.destroy!
     end
+
+    redirect_to users_path, notice: "#{@user.name} successfully deleted."
+  rescue ActiveRecord::RecordInvalid, ActiveRecord::RecordNotDestroyed
+    redirect_to users_path, alert: "Couldn't delete user"
   end
 
   private
@@ -44,5 +50,14 @@ class UsersController < ApplicationController
 
   def user_params
     %i[name email organisation_id password password_confirmation]
+  end
+
+  def fallback_admin_for(user)
+    Admin.where.not(id: user.id).order(:id).first
+  end
+
+  def reassign_lessons_to_fallback(user, fallback_admin)
+    Lesson.where(assigned_editor_id: user.id).update_all(assigned_editor_id: fallback_admin.id)
+    Lesson.where(creator_id: user.id).update_all(creator_id: fallback_admin.id)
   end
 end
