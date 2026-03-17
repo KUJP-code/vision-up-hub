aaa92736275aa31861c3e5e0793dcfd25cb9f554
jaysonpye <jaysonpye@hotmail.com>
2026-03-17 13:49:30 +0900

    add org check

--- spec/models/school_teacher_spec.rb
+++ spec/models/school_teacher_spec.rb
@@ -4,6 +4,18 @@ require 'rails_helper'
 
 RSpec.describe SchoolTeacher do
   it 'has a valid factory' do
-    expect(build(:school_class)).to be_valid
+    expect(build(:school_teacher)).to be_valid
+  end
+
+  it 'is invalid when the school belongs to a different organisation than the teacher' do
+    teacher = create(:user, :teacher)
+    other_school = create(:school)
+
+    school_teacher = build(:school_teacher, teacher:, school: other_school)
+
+    expect(school_teacher).not_to be_valid
+    expect(school_teacher.errors[:school]).to include(
+      'must belong to the same organisation as the teacher'
+    )
   end
 end
