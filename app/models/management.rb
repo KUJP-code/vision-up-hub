8366b080e75d6d52d82958eec89c140e65f19f6a
jaysonpye <jaysonpye@hotmail.com>
2026-03-17 13:48:57 +0900

    add org guards to management models

--- app/models/management.rb
+++ app/models/management.rb
@@ -2,6 +2,7 @@
 
 class Management < ApplicationRecord
   validate :sm_is_sm
+  validate :school_manager_school_organisation_matches
 
   belongs_to :school
   belongs_to :school_manager
@@ -10,5 +11,12 @@ class Management < ApplicationRecord
 
   def sm_is_sm
     errors.add(:user, 'is not a school manager') unless school_manager.is?('SchoolManager')
+  end
+
+  def school_manager_school_organisation_matches
+    return if school.blank? || school_manager.blank?
+    return if school.organisation_id == school_manager.organisation_id
+
+    errors.add(:school, 'must belong to the same organisation as the school manager')
   end
 end
--- app/models/school_teacher.rb
+++ app/models/school_teacher.rb
@@ -3,4 +3,15 @@
 class SchoolTeacher < ApplicationRecord
   belongs_to :school
   belongs_to :teacher
+
+  validate :teacher_school_organisation_matches
+
+  private
+
+  def teacher_school_organisation_matches
+    return if school.blank? || teacher.blank?
+    return if school.organisation_id == teacher.organisation_id
+
+    errors.add(:school, 'must belong to the same organisation as the teacher')
+  end
 end
