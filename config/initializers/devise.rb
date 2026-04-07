ed1afff24be652ac75485eb896acb9adc6e0f87d
jaysonpye <jaysonpye@hotmail.com>
2026-04-07 10:17:41 +0900

    change admin res

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -9,7 +9,7 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
-  config.expire_password_after = 1.day
+  config.expire_password_after = 30.days
 
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
