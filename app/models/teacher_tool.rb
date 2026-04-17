818d2466cc8f88f1ae6ac525eb55ec8ae9d97c96
jaysonpye <jaysonpye@hotmail.com>
2026-04-17 12:13:07 +0900

    add multi link to teacher tools

--- app/models/teacher_tool.rb
+++ app/models/teacher_tool.rb
@@ -6,35 +6,56 @@ class TeacherTool < ApplicationRecord
 
   enum :kind, { video: 0, external: 1 }, default: :video
 
+  before_validation :clear_video_fields_for_external_tools, if: :external?
+
   validates :title, presence: true
   validates :title, uniqueness: { scope: :organisation_id }
-  validates :embed_url, presence: true, if: :video?
   validates :url, presence: true, if: :external?
   validates :position, numericality: { greater_than_or_equal_to: 0 }
-  validate :sanitize_video_path, if: :video?
+  validate :sanitize_video_paths, if: :video?
+  validate :video_paths_present, if: :video?
 
   def effective_embed_url
-    embed_url.presence || url
+    random_embed_url.presence || embed_url.presence || url
   end
 
   def video_path
-    embed_url
+    video_paths_list.first || embed_url
   end
 
   def video_path=(value)
-    self.embed_url = value
+    self.video_paths = Array(value).compact_blank
+    self.embed_url = self.video_paths.first
+  end
+
+  def video_paths_list
+    Array(video_paths).compact_blank
+  end
+
+  def random_embed_url
+    video_paths_list.sample
   end
 
   private
 
-  def sanitize_video_path
-    return if embed_url.blank?
+  def sanitize_video_paths
+    raw_paths = video_paths_list
+    raw_paths = [embed_url] if raw_paths.empty? && embed_url.present?
+    return if raw_paths.empty?
 
-    self.embed_url = normalize_video_url(embed_url)
+    normalized_paths = raw_paths.map { |value| normalize_video_url(value) }
+    self.video_paths = normalized_paths
+    self.embed_url = normalized_paths.first
   rescue URI::InvalidURIError
-    errors.add(:video_path, "#{embed_url} is not a valid URL")
+    errors.add(:video_paths, "#{raw_paths.find(&:present?)} is not a valid URL")
   rescue ArgumentError
-    errors.add(:video_path, "#{embed_url} is not supported")
+    errors.add(:video_paths, "#{raw_paths.find(&:present?)} is not supported")
+  end
+
+  def video_paths_present
+    return if video_paths_list.any? || embed_url.present?
+
+    errors.add(:video_paths, "can't be blank")
   end
 
   def normalize_video_url(value)
@@ -58,5 +79,10 @@ class TeacherTool < ApplicationRecord
     else
       raise ArgumentError
     end
+  end
+
+  def clear_video_fields_for_external_tools
+    self.video_paths = []
+    self.embed_url = nil
   end
 end
