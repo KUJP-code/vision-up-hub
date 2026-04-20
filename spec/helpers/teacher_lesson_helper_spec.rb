ecc57363da962f7961359222b7760d5c171378e4
jaysonpye <jaysonpye@hotmail.com>
2026-04-20 11:21:24 +0900

    add spc to teacher helper

--- spec/helpers/teacher_lesson_helper_spec.rb
+++ spec/helpers/teacher_lesson_helper_spec.rb
@@ -12,22 +12,34 @@ RSpec.describe TeacherLessonHelper do
     end
 
     it 'renders specialist evening class cards in the configured subtype order' do
-      literacy = create(:evening_class, level: :specialist, subtype: :literacy)
       project_session_2 = create(:evening_class, level: :specialist, subtype: :project_session_2)
-      relation = Lesson.where(id: [project_session_2.id, literacy.id])
+      special_lesson = create(:evening_class, level: :specialist, subtype: :special_lesson)
+      relation = Lesson.where(id: [special_lesson.id, project_session_2.id])
 
       cards = helper.teacher_lesson_cards('specialist', relation)
       evening_cards = cards.select { |card| card.type == 'EveningClass' }
 
-      expect(evening_cards.map(&:subtype)).to eq(%w[literacy project_session_2])
+      expect(evening_cards.map(&:subtype)).to eq(%w[project_session_2 special_lesson])
     end
 
-    it 'does not include legacy specialist resource cards' do
+    it 'ignores legacy specialist evening lesson subtypes' do
       literacy = create(:evening_class, level: :specialist, subtype: :literacy)
 
       cards = helper.teacher_lesson_cards('specialist', Lesson.where(id: literacy.id))
 
-      expect(cards.select { |card| card.kind == :resource }.map(&:type)).to be_empty
+      expect(cards.select { |card| card.type == 'EveningClass' }).to be_empty
+    end
+
+    it 'includes specialist literacy and discussion resource cards around evening class cards' do
+      project_session_1 = create(:evening_class, level: :specialist, subtype: :project_session_1)
+
+      cards = helper.teacher_lesson_cards('specialist', Lesson.where(id: project_session_1.id))
+
+      expect(cards.map { |card| [card.kind, card.type, card.subtype] }).to eq([
+        [:resource, 'sp_literacy', nil],
+        [:lesson, 'EveningClass', 'project_session_1'],
+        [:resource, 'sp_discussion', nil]
+      ])
     end
 
     it 'does not include the hard-coded keep up conversation time resource card' do
@@ -54,8 +66,18 @@ RSpec.describe TeacherLessonHelper do
 
   describe '#teacher_lesson_card_title' do
     it 'uses the subtype label for evening class subtype cards' do
-      expect(helper.teacher_lesson_card_title('EveningClass', level: 'specialist', subtype: 'discussion'))
-        .to eq(I18n.t('lessons.subtypes.discussion'))
+      expect(helper.teacher_lesson_card_title('EveningClass', level: 'specialist', subtype: 'project_session_2'))
+        .to eq(I18n.t('lessons.subtypes.project_session_2'))
+    end
+
+    it 'uses the teacher-facing label for specialist literacy resource cards' do
+      expect(helper.teacher_lesson_card_title('sp_literacy', level: 'specialist'))
+        .to eq(I18n.t('teacher_resources.index.sp_literacy'))
+    end
+
+    it 'uses the teacher-facing label for specialist discussion resource cards' do
+      expect(helper.teacher_lesson_card_title('sp_discussion', level: 'specialist'))
+        .to eq(I18n.t('teacher_resources.index.sp_discussion'))
     end
   end
 end
